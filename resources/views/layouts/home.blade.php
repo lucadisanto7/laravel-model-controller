e48765f2e83e81a4c0b8450e20b33821919adbaf
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-28 16:06:09 +0200

    [UPD] Deleted welcome blade and added guest blade and home blade

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts-guest')
+
+@section('contents')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">Movies</h1>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/movies', [PageController::class, 'index'])->name('movies');
