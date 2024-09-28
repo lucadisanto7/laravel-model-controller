9e5179da54b1a8aac3b5e4793f52e62290acbcfe
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-28 16:10:08 +0200

    [UPD] Added foreach in home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,6 +6,20 @@
         <div class="col-12">
             <h1 class="text-center">Movies</h1>
         </div>
+        <div class="col-12">
+            <div class="row">
+                @foreach ($movies as $movie)
+                <div class="card">
+                       <div class="card-title">
+                           <h4>{{ $movie->title }}</h4>
+                       </div>
+                        <div class="card-body">
+                            <p>{{ $movie->original_title }}, {{$movie->nationality}}, {{ $movie->vote }}, {{ $movie->date }}</p>
+                        </div>
+                    </div> 
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
 @endsection
