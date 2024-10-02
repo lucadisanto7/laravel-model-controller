e3a866c7f0e88568a9a88346e11cf7bd53d5a62e
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-02 09:30:52 +0200

    [FIX] Fixed route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Guest\PageController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/movies', [PageController::class, 'index'])->name('movies');
